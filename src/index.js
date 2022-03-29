5f13cf211934510ab3169be63021ebd3c98435c8
Darren Nelson <nelson.darrenp10.dnp@gmail.com>
2022-03-29 09:35:39 +0200

    My first react app

--- src/index.js
+++ src/index.js
@@ -1,15 +1,71 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './styles.css';
+import reportWebVitals from './reportWebVitals'; // This is for performance testing right?
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+import profilePicture from './displayPicture.jpg';
+
+// importing from react-bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
+const user = {
+  name: 'Darren',
+  surname: 'Nelson',
+  dob: '21 May 1997',
+  country: 'South Africa',
+  email: 'nelson.darrenp10.dnp@gmail.com',
+  telephone: '071-482-4330',
+  company: 'Edge Education',
+  profilePicture: profilePicture,
+  interests: ['soccer', 'hiking', 'surfing', 'clubbing', 'gaming', 'camping']
+};
+
+const returnInfo = (key) => {
+  let info = user[key];
+  let informationOutput = document.getElementById('informationOutput');
+
+  let renderInfo = '';
+
+  if (typeof info === 'object') {
+    renderInfo = (
+      <ul className="interestList">
+        {Object.values(info).map((value, key) => (
+          <li key={key}>{value}</li>
+        ))}
+      </ul>
+    );
+  } else if (key == 'profilePicture') {
+    renderInfo = <img src={info}></img>;
+  } else {
+    renderInfo = info;
+  }
+
+  ReactDOM.render(renderInfo, informationOutput);
+};
+
+const buttons = (
+  <div>
+    <div className="buttonContainer">
+      {Object.keys(user).map((value, key) => (
+        <Button
+          variant="secondary"
+          onClick={(e) => returnInfo(value, e)}
+          key={key}
+        >
+          {value}
+        </Button>
+      ))}
+    </div>
+
+    <div id="informationOutput" className="informationOutput">
+      Hello World, click a button above to find out more about the user...
+    </div>
+  </div>
 );
+
+ReactDOM.render(buttons, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
